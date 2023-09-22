5fa3c3e186ffc9c86ade69ff2a67552a2070a6cf
rafikxhawshar <rafikhawshar@gmail.com>
2023-09-22 10:37:53 +0300

    Basic Quiries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+Answer 1 : SELECT name FROM  students
+Answer 2 : SELECT * FROM students WHERE Age>30;
+Answer 3 : SELECT name FROM students WHERE Age>30 AND Gender="F"
+Answer 4 : SELECT Points FROM students WHERE name="Alex"
+Answer 5 : INSERT INTO students(ID,name,Age,Gender,Points) VALUES("7","Rafik","18","M","560")
+Answer 6 : UPDATE students SET Points="500" WHERE name="Basma"
+Answer 7 : UPDATE students SET Points="100" WHERE name="Alex"
