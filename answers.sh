cf3ebce0e99d6cdba1a225f4ae3b4420899f4eea
rafikxhawshar <rafikhawshar@gmail.com>
2023-09-22 10:52:54 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -10,3 +10,13 @@ CREATE TABLE  graduates(ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT 
 Answer 10 : INSERT INTO graduates (name,Age,Gender,Points) SELECT name,Age,gender,Points FROM students WHERE name="Layal";
 Answer 11 : UPDATE graduates SET Graduation= "08/09/2018" WHERE name="Layal";
 Answer 12 : DELETE  FROM students WHERE name="Layal";
+
+Answe 14 : SELECT employees.Name, employees.Company, companies.date
+FROM employees, companies
+WHERE employees.Company = companies.name ;
+SELECT employees.name 
+FROM employees INNER JOIN companies ON employees.Company=companies.Name
+Answer 15 :  WHERE companies.Date<"2000";
+SELECT companies.name 
+FROM companies INNER JOIN employees ON employees.Company=companies.name
+Answer 16 : WHERE employees.Role="Graphic Designer";
